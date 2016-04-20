29e80c1c41d6a1216251cc3318b0138ad3166ca5
friendllcc <759894921@qq.com>
2016-04-20 17:51:13 +0800

    improve

--- js/pingwest.js
+++ js/pingwest.js
@@ -26,11 +26,78 @@ $(".indexBannerTextHot").click(function(){
 });
 
 $(".indexAbout").hover(function(){
-	$(this).children("span").animate({bottom:'-100px'},500);
+	$(this).children("span").stop(true, false).animate({bottom:'-100px'},500);
 }, function(){
-	$(this).children("span").animate({bottom:'0px'},500);
+	$(this).children("span").stop(true, false).animate({bottom:'0px'},500);
 });
 
+
+
+//数据可更新
+// var a1 = $(".indexMainSlideRight").children();
+// for (var i = 0; i < a1.length;i++){
+//     var number = (i+1)%7;
+//     if(number==1){
+//         a1[i].style.width="calc(41.6666666% - 22px)";
+//         a1[i].style.height="280px";
+//     }
+//     if(number==2){
+//         a1[i].style.width="calc(25% - 22px)";
+//         a1[i].style.height="250px";
+//     }
+//     if(number==3){
+//         a1[i].style.width="calc(33.3333333% - 22px)";
+//         a1[i].style.height="280px";
+//     }
+//     if(number==4){
+//         a1[i].style.width="calc(25% - 22px)";
+//         a1[i].style.height="250px";
+//     }
+//     if(number==5){
+//         a1[i].style.width="calc(50% - 22px)";
+//         a1[i].style.height="250px";
+//     }
+//     if(number==6){
+//         a1[i].style.width="calc(25% - 22px)";
+//         a1[i].style.height="250px";
+//     }
+//     if(number==0){
+//         a1[i].style.width="calc(45% - 22px)";
+//         a1[i].style.height="250px";
+//     }
+// } 
+// var b1 = $(".indexMainSlideLeft").children();
+// for (var i = 0; i < b1.length;i++){
+//     var number = (i+1)%7;
+//     if(number==1){
+//         b1[i].style.width="calc(41.6666666% - 22px)";
+//         b1[i].style.height="280px";
+//     }
+//     if(number==2){
+//         b1[i].style.width="calc(25% - 22px)";
+//         b1[i].style.height="250px";
+//     }
+//     if(number==3){
+//         b1[i].style.width="calc(33.3333333% - 22px)";
+//         b1[i].style.height="280px";
+//     }
+//     if(number==4){
+//         b1[i].style.width="calc(25% - 22px)";
+//         b1[i].style.height="250px";
+//     }
+//     if(number==5){
+//         b1[i].style.width="calc(50% - 22px)";
+//         b1[i].style.height="250px";
+//     }
+//     if(number==6){
+//         b1[i].style.width="calc(25% - 22px)";
+//         b1[i].style.height="250px";
+//     }
+//     if(number==0){
+//         b1[i].style.width="calc(45% - 22px)";
+//         b1[i].style.height="250px";
+//     }
+// } 
 
 // 弹出框jQuery
 
