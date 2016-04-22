e840abeaf4108c7276e397b0d6fd5cbea2fac83b
friendllcc <759894921@qq.com>
2016-04-22 12:24:00 +0800

    improve

--- js/pingwest.js
+++ js/pingwest.js
@@ -1,23 +1,23 @@
 
 $(".navSlideDown").slideUp();
 $(".navBlock").hover(function(){
-	$(this).children(".navSlideDown").stop(true, false).slideDown();
+    $(this).children(".navSlideDown").stop(true, false).slideDown();
 }, function(){
-	$(this).children(".navSlideDown").stop(true, false).slideUp();
+    $(this).children(".navSlideDown").stop(true, false).slideUp();
 });
 
 
 
 $(".indexBannerTextHot").css("background","#ff5624");
 $(".indexMainSlideLeft").css("display","none");
-$(".indexBannerTextDongtai").click(function(){
+$(".indexBannerTextDongtai").on("click",function(){
 	$(".indexBannerTextDongtai").css("background","#ff5624");
 	$(".indexBannerTextHot").css("background","#D4D4D4");
     $(".indexMainSlideRight").fadeOut("show", function(){
 	$(".indexMainSlideLeft").fadeIn("show");
 });
 });
-$(".indexBannerTextHot").click(function(){
+$(".indexBannerTextHot").on("click",function(){
 	$(".indexBannerTextDongtai").css("background","#D4D4D4");
 	$(".indexBannerTextHot").css("background","#ff5624");
     $(".indexMainSlideLeft").fadeOut("show", function(){
@@ -32,72 +32,6 @@ $(".indexAbout").hover(function(){
 });
 
 
-
-//数据可更新
-// var a1 = $(".indexMainSlideRight").children();
-// for (var i = 0; i < a1.length;i++){
-//     var number = (i+1)%7;
-//     if(number==1){
-//         a1[i].style.width="calc(41.6666666% - 22px)";
-//         a1[i].style.height="280px";
-//     }
-//     if(number==2){
-//         a1[i].style.width="calc(25% - 22px)";
-//         a1[i].style.height="250px";
-//     }
-//     if(number==3){
-//         a1[i].style.width="calc(33.3333333% - 22px)";
-//         a1[i].style.height="280px";
-//     }
-//     if(number==4){
-//         a1[i].style.width="calc(25% - 22px)";
-//         a1[i].style.height="250px";
-//     }
-//     if(number==5){
-//         a1[i].style.width="calc(50% - 22px)";
-//         a1[i].style.height="250px";
-//     }
-//     if(number==6){
-//         a1[i].style.width="calc(25% - 22px)";
-//         a1[i].style.height="250px";
-//     }
-//     if(number==0){
-//         a1[i].style.width="calc(45% - 22px)";
-//         a1[i].style.height="250px";
-//     }
-// } 
-// var b1 = $(".indexMainSlideLeft").children();
-// for (var i = 0; i < b1.length;i++){
-//     var number = (i+1)%7;
-//     if(number==1){
-//         b1[i].style.width="calc(41.6666666% - 22px)";
-//         b1[i].style.height="280px";
-//     }
-//     if(number==2){
-//         b1[i].style.width="calc(25% - 22px)";
-//         b1[i].style.height="250px";
-//     }
-//     if(number==3){
-//         b1[i].style.width="calc(33.3333333% - 22px)";
-//         b1[i].style.height="280px";
-//     }
-//     if(number==4){
-//         b1[i].style.width="calc(25% - 22px)";
-//         b1[i].style.height="250px";
-//     }
-//     if(number==5){
-//         b1[i].style.width="calc(50% - 22px)";
-//         b1[i].style.height="250px";
-//     }
-//     if(number==6){
-//         b1[i].style.width="calc(25% - 22px)";
-//         b1[i].style.height="250px";
-//     }
-//     if(number==0){
-//         b1[i].style.width="calc(45% - 22px)";
-//         b1[i].style.height="250px";
-//     }
-// } 
 
 // 弹出框jQuery
 
